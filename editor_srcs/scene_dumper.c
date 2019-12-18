6da440b56c41e1f56bd7588a8b9d334def6ce7d3
M'bark Erras <merras@e1r3p8.1337.ma>
2019-12-18 04:10:22 +0100

    refactoring dumping code

--- editor_srcs/scene_dumper.c
+++ editor_srcs/scene_dumper.c
@@ -22,44 +22,19 @@
         - scene_dumper() returns != 0 if an error occured during dumping
 */
 
-int     dump_sprites(int fd, t_list_node *sprites)
+int     list_dumper(int fd, t_list_node *nodes, int content_size)
 {
     int err;
 
     err = 0;
-    while (sprites)
+    while (nodes)
     {
-        err = write(fd, CAST(sprites, t_sprite), sizeof(t_sprite));
-        sprites = sprites->next;
-    }
-    return(err);
-}
-
-int     dump_portals(int fd, t_list_node *portals)
-{
-    int err;
-
-    err = 0;
-    while (portals)
-    {
-        err = write(fd, CAST(portals, t_portal), sizeof(t_portal));
-        portals = portals->next;
+        err = write(fd, nodes->content, content_size);
+        nodes = nodes->next;
     }
     return (err);
 }
 
-int     dump_walls(int fd, t_list_node *walls)
-{
-    int err;
-
-    err = 0;
-    while (walls)
-    {
-        err = write(fd, CAST(walls, t_wall), sizeof(t_wall));
-        walls = walls->next;
-    }
-    return (err);
-}
 int    dump_sectors(int fd, t_list_node *sectors)
 {
     int err;
@@ -72,15 +47,15 @@ int    dump_sectors(int fd, t_list_node *sectors)
         err = write(fd, &CAST(sectors, t_sector)->props, sizeof(uint32_t));
         err = write(fd, &CAST(sectors, t_sector)->floor_texture, sizeof(uint32_t));
         err = write(fd, &CAST(sectors, t_sector)->ceil_texture, sizeof(uint32_t));
-        dump_walls(fd, CAST(sectors, t_sector)->walls.first);
-        dump_portals(fd, CAST(sectors, t_sector)->walls.first);
-        dump_sprites(fd, CAST(sectors, t_sector)->walls.first);
+        list_dumper(fd, CAST(sectors, t_sector)->walls.first, sizeof(t_wall));
+        list_dumper(fd, CAST(sectors, t_sector)->walls.first, sizeof(t_portal));
+        list_dumper(fd, CAST(sectors, t_sector)->walls.first, sizeof(t_sprite));
         sectors = sectors->next;
     }
     return (err);
 }
 
-int     scene_dumper(t_graphical_scene scene)
+int     scene_dumper(t_graphical_scene scene, t_graphical_settings settings)
 {
     int         fd;
     int         err;
@@ -90,5 +65,6 @@ int     scene_dumper(t_graphical_scene scene)
         return (OPEN_ERROR);
     err = write(fd, &scene.current_sector->index, CSECTOR);
     err = dump_sectors(fd, scene.sectors.first);
+    err = dump_textures(fd, settings.textures);
     return (err);
 }
